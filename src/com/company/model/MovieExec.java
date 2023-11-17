aa564b223053ffb86f4012d3c2ba04a53117d691
smg8710 <tedy13082005@gmail.com>
2023-11-17 15:44:07 +0200

    dddd

---
+++ src/com/company/model/MovieExec.java
@@ -0,0 +1,8 @@
+package com.company.model;
+
+public class MovieExec {
+    private String name;
+    private String address;
+    private int networth;
+
+}
---
+++ src/com/company/model/StarSin.java
@@ -0,0 +1,9 @@
+package com.company.model;
+
+public class StarSin {
+    public class StarsIn {
+        private String movieTitle;
+        private int year;
+        private MovieStar movieStar;
+    }
+}
